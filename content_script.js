411503a9c23636ebd7add415dee93f98d7eae8ef
centipeda <joshua.c.guam@gmail.com>
2019-11-08 21:06:51 -0500

    attempt to use Boundary

--- content_script.js
+++ content_script.js
@@ -1,2 +1,8 @@
-console.log("testing");
-$("testing stuff").appendTo('body');
+console.log("testing"); 
+$.get(chrome.extension.getURL("news_box.html"), function(data) {
+    console.log("getting data");
+    // $("#body").prepend(data);
+    newsbox = Boundary.createBox("mainbox");
+    Boundary.appendToBox("#mainbox", data);
+    Boundary.loadBoxCSS("#mainbox", chrome.extension.getURL("ext/material.min.css"));
+});
