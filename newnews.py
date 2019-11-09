f1547d3cfb86e70baaefd81293124fc6bc12de4b
centipeda <joshua.c.guam@gmail.com>
2019-11-09 12:48:09 -0500

    h

---
+++ newnews.py
@@ -0,0 +1,8 @@
+import newspaper
+import newsapi
+import sys
+
+url = "https://www.npr.org/2019/11/09/777587890/the-cozy-snowbound-sweater-wearing-guide-to-2019-holiday-movies"
+
+client = NewsApiClient(api_key='0cfc6dadf31e40d588afd3cbb5020b09')
+
