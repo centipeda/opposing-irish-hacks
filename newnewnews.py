41592116e9980d7efacb9d62c52f2f1a758f6045
centipeda <joshua.c.guam@gmail.com>
2019-11-09 14:53:19 -0500

    h

--- newnewnews.py
+++ newnewnews.py
@@ -5,10 +5,15 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 authenticator = IAMAuthenticator('7AQhKTuRCG_tEM68269k1zZiQFQbovXHGNIX_Q1kxyWT')
 discovery = DiscoveryV1(
-    version='2018-08-01',
-    authenticator=authenticator)
+    version='2019-04-30',
+    authenticator=authenticator
+)
+
 discovery.set_service_url('https://gateway-syd.watsonplatform.net/discovery/api')
 
-environments = discovery.list_environments().get_result()
-print(json.dumps(environments, indent=2))
-
+with open('./test-doc1.html') as fileinfo:
+    add_doc = discovery.add_document(
+        '{environment_id}',
+        '{collection_id}',
+        file=fileinfo).get_result()
+print(json.dumps(add_doc, indent=2))
