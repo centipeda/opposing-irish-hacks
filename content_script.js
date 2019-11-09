f79b8e46cf310c645c6937f149c83798329074c9
centipeda <joshua.c.guam@gmail.com>
2019-11-09 16:32:00 -0500

    h

--- content_script.js
+++ content_script.js
@@ -23,15 +23,18 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
             // result will be a list of objects that have
             // properties like: url, title...
             class Article {
-                constructor(u, t) {
+                constructor(u, t, b) {
                     this.url = u;
                     this.title = t;
+                    this.bias = b;
                 }
             }
-            article = {
-                url: "https://www.npr.org/2019/11/09/777914177/republicans-ask-for-whistleblower-hunter-biden-to-testify-in-impeachment-inquiry",
-                title: "Republicans Ask For Whistleblower, Hunter Biden To Testify In Impeachment Inquiry"
-            }
+            
+            article1 = new Article ("https://www.foxnews.com/politics/trump-brushes-off-witch-hunt-impeachment-probe", "Trump wants Pelosi, Biden to be called as impeachment inquiry witnesses, says he'll release second phone call transcript", "right");
+            article2 = new Article ("https://www.nbcnews.com/politics/trump-impeachment-inquiry/only-3-senate-republicans-aren-t-defending-trump-impeachment-inquiry-n1078906", "Only 3 Senate Republicans aren't defending Trump from the impeachment inquiry. Here's why.", "left");
+            article3 = new Article ("https://www.cbsnews.com/news/trump-impeachment-inquiry-recap-house-releases-testimony-transcripts-prepares-public-hearings-2019-11-09/", "What happened in the impeachment inquiry this week", "neutral");
+
+            var articles = [article1, article2, article3];
 
             console.log(result);
         }
