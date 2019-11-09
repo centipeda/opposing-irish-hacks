d424b13a8f523e0e99b839aabca18ab905cdaee4
centipeda <joshua.c.guam@gmail.com>
2019-11-08 21:26:50 -0500

    get more stuff working

--- content_script.js
+++ content_script.js
@@ -1,8 +1,5 @@
 console.log("testing"); 
 $.get(chrome.extension.getURL("news_box.html"), function(data) {
     console.log("getting data");
-    // $("#body").prepend(data);
-    newsbox = Boundary.createBox("mainbox");
-    Boundary.appendToBox("#mainbox", data);
-    Boundary.loadBoxCSS("#mainbox", chrome.extension.getURL("ext/material.min.css"));
+    $("body").prepend(data);
 });
