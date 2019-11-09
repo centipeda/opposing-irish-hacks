ab6c76259b87f6f788411ea15c9f3b20ce7226fb
centipeda <joshua.c.guam@gmail.com>
2019-11-09 16:21:23 -0500

    override styles?

--- content_script.js
+++ content_script.js
@@ -22,6 +22,12 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
         }).then( (res) => res.json()).then( (result) => {
             // result will be a list of objects that have
             // properties like: url, title...
+            class Article {
+                constructor(u, t) {
+                    this.url = u;
+                    this.title = t;
+                }
+            }
             article = {
                 url: "https://www.npr.org/2019/11/09/777914177/republicans-ask-for-whistleblower-hunter-biden-to-testify-in-impeachment-inquiry",
                 title: "Republicans Ask For Whistleblower, Hunter Biden To Testify In Impeachment Inquiry"
