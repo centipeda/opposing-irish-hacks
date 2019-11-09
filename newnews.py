25eaf493c30900d04f138998d3c4efee1d484e0a
centipeda <joshua.c.guam@gmail.com>
2019-11-09 13:08:32 -0500

    h

--- newnews.py
+++ newnews.py
@@ -9,18 +9,21 @@ article.parse()
 article.nlp()
 keywords = article.keywords
 
-print("keywords: " + " OR ".join(keywords))
+query = " OR ".join(keywords)
+
+print("keywords: " + query)
 
 client = NewsApiClient(api_key='0cfc6dadf31e40d588afd3cbb5020b09')
 
-all_articles = client.get_everything(q=' OR '.join(keywords),
+all_articles = client.get_everything(q=query,
                                       sources='bbc-news, cnn, fox-news',
                                       from_param='2019-11-01',
-                                      to='2019-11-09',
+                                      to='2019-11-15',
                                       language='en',
-                                      sort_by='relevancy',
-                                      page=2)
+                                      sort_by='re'
+                                      )
 
-# links = [a for a in all_articles.articles]
-print(all_articles['articles'][0].url)
+links = [a['url'] for a in all_articles['articles']]
+for l in links[0:3]:
+    print(l)
 
