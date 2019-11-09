6d4a5e0574bfae19b7edb516c104dcf901607b6b
centipeda <joshua.c.guam@gmail.com>
2019-11-08 22:14:39 -0500

    agAIN

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if(changeInfo.status == "complete") {
         console.log("finished loading");
-        console.log(changeInfo.url);
+        //console.log(changeInfo.url);
     }
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
         var url = tabs[0].url;
         console.log(url);
     });
