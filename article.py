be31a7df4d8026fc0b2d24d9c6842d8d3e8325ce
centipeda <joshua.c.guam@gmail.com>
2019-11-09 11:43:54 -0500

    h

--- article.py
+++ article.py
@@ -38,25 +38,32 @@ right_query[:-2]
 
 #Check which list site is in and filter query to sites from
 #other two lists
-for site in center:
-    if site in url:
-        for j in search(right_query, tld="co.in", num = 20, pause = 2):
-            print(j)
-        for j in search(left_query, tld="co.in", num=20, pause=2):
-            print(j)
+alreadyFound = False
 
-for site in right:
-    if site in url:
-        query = left_query + center_query
+if not alreadyFound:
+    for site in center:
+        if site in url:
+            alreadyFound = True
+            for j in search(right_query, tld="co.in", num = 20, pause = 2):
+                print(j)
+            for j in search(left_query, tld="co.in", num=20, pause=2):
+                print(j)
 
-for site in left:
-    if site in url:
-        query = right_query + center_query
+if not alreadyFound:
+    for site in right:
+        if site in url:
+            alreadyFound = True
+            for j in search(left_query, tld="co.in", num = 20, pause = 2):
+                print(j)
+            for j in search(center_query, tld="co.in", num=20, pause=2):
+                print(j)
 
+if not alreadyFound:
+    for site in left:
+        if site in url:
+            alreadyFound = True
+            for j in search(right_query, tld="co.in", num = 20, pause = 2):
+                print(j)
+            for j in search(center_query, tld="co.in", num=20, pause=2):
+                print(j)
 
-
-
-
-
-for j in search(query, tld="co.in", num=20, pause=2): 
-    print(j) 
