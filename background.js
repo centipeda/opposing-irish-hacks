dcd60178b8d3d4f321b185b8169d192b6aa612d6
centipeda <joshua.c.guam@gmail.com>
2019-11-08 22:09:48 -0500

    again

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if(changeInfo.status == "complete") {
         console.log("finished loading");
+        console.log(changeInfo.url);
     }
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
